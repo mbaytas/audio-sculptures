96ee98efff018ad77a24d8e9625bc0adb604aa73
Mehmet Aydın Baytaş <mbaytas@Lumos-III.local>
2016-02-29 19:30:50 +0200

    Some edits on spectrobars

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # audio-sculptures
 
-This repository contains Python scripts for generating 3D models that represent audio information. These models can be fabricated into objects.
+This repository contains Python scripts for generating 3D models that represent audio information. These models can then be fabricated into objects.
 
 Many people before me have worked on code that generates 3D models from audio. Below are some projects I've found, in no particular order:
 
@@ -11,20 +11,26 @@ Many people before me have worked on code that generates 3D models from audio. B
 - Realit&auml;t's [Microsonic Landscapes](http://www.realitat.com/microsonic/)
 - Gene Kogan's [Spectrogram Sculptures](http://www.genekogan.com/works/audio-sculpture.html)
 
-So why did I feel the need to write these scripts? Basically, while each of the above projects is awesome in its own way, I wanted to have:
+While each of the above projects is awesome in its own way, I wanted to have:
 
 - A completely open source script...
 - ...in Python...
 - ...that takes an audio file and generate a more or less 3D printer-ready STL file in only one step...
-- ...while preserving an accurate representation of the sonic information, so the object should be able to be "played back" to produce (a hopelessly downsampled and bit-reduced version of) the same audio.
+- ...while preserving an accurate representation of the sonic information, so the object can be "played back" to produce (a hopelessly downsampled and bit-reduced version of) the same audio.
 
 I decided to produce this script iteratively. You can find an account of my attempts below.
 
 ## Attempt 1: spectrobars
 ### Rectangular Bar Graph using [tangible](https://github.com/dbrgn/tangible)
 
+#### To Do
+
+- This script generates a SCAD file, rather than the 3D printer-ready STL file. I can then convert this file into an STL using OpenSCAD. However I'd like to generate an STL in one step.
+
 ## Attempt 2: spectropie
 ### Concentric Pie Graphs using [tangible](https://github.com/dbrgn/tangible)
+
+Coming soon...
 
 ## Attempt 3: spectrosmooth
 ### Smooth Concentric Pie Graphs using [numpystl](https://github.com/WoLpH/numpy-stl)
--- spectrobars.py
+++ spectrobars.py
@@ -6,45 +6,50 @@ from tangible.backends.openscad import OpenScadBackend
 from tangible.shapes.bars import BarsND
 import wave
 
-def main(filename):
-    print "Reading and analyzing file..."
-    spectrum, freqs = ReadAndAnalyze(filename)
 
-    print len(spectrum[0])
-    print len(freqs)
+wav_file = 'meantime.wav'
+loggishness = 0.00000000000004
+nfft = 2**18
+padto = nfft/(2**13)
 
-    print "Scaling logarithmically (kinda)..."
-    for i, s in enumerate(spectrum):
-      spectrum[i] = map(loggish, s)
+print "Reading and analyzing file..."
+spectrum, freqs = ReadAndAnalyze(filename)
 
-    print "Generating linear scale..."
-    scale = scales.linear(domain=[spectrum.min(), spectrum.max()],
-                          codomain=[1, 10])
+print len(spectrum[0])
+print len(freqs)
 
-    print "Normalizing spectrum data..."
-    datapoints = map(lambda x: map(scale, x), spectrum)
+print "Scaling logarithmically (kinda)..."
+for i, s in enumerate(spectrum):
+  spectrum[i] = map(loggish, s)
 
-    print "Trimming spectrum data post-normalization..."
-    for i, x in enumerate(datapoints):
-      for j, v in enumerate(x):
-          if v > 9:
-            datapoints[i][j] = 9
+print "Generating linear scale..."
+scale = scales.linear(domain=[spectrum.min(), spectrum.max()],
+                      codomain=[1, 10])
 
-    print "Generating bars..."
-    bars = BarsND(datapoints,
-                  bar_width=1,
-                  bar_depth=1)
+print "Normalizing spectrum data..."
+datapoints = map(lambda x: map(scale, x), spectrum)
 
-    print "Rendering..."
-    code = bars.render(backend=OpenScadBackend)
+print "Trimming spectrum data post-normalization..."
+for i, x in enumerate(datapoints):
+  for j, v in enumerate(x):
+      if v > 9:
+        datapoints[i][j] = 9
 
-    print "Saving to file..."
-    with open("bars.scad", "w") as f:
-        f.write(code)
+print "Generating bars..."
+bars = BarsND(datapoints,
+              bar_width=1,
+              bar_depth=1)
+
+print "Rendering..."
+code = bars.render(backend=OpenScadBackend)
+
+print "Saving to file..."
+with open("bars.scad", "w") as f:
+    f.write(code)
 
 def loggish(v):
   v = float(v)
-  L = 0.00000000000004
+  L = loggishness
   return L * (log(v) - v) + v
 
 def ReadAndAnalyze(filename):
@@ -53,17 +58,10 @@ def ReadAndAnalyze(filename):
     sound_info = numpy.fromstring(frames, 'Int16')
     frame_rate = wav.getframerate()
     wav.close()
-    nfft = 2**18
-    padto = nfft/(2**13)
     specdata = mlab.specgram(sound_info,
                              NFFT=nfft,
                              pad_to=padto,
                              Fs=frame_rate)
-                             #noverlap=512)
     spectrum = specdata[0]
     freqs = specdata[1]
     return spectrum, freqs
-
-if __name__ == '__main__':
-    wav_file = 'meantime.wav'
-    main(wav_file)
